50ba2a2cc0b56ee4231d4a30afe3a19eb741b0a9
egret walkerrrr <moonmix@protonmail.com>
2022-03-10 17:22:35 +0100

    fix favicon.ico support but still sth to do

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,4 +33,7 @@ DEFAULT_PAGINATION = False
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-EXTRA_PATH_METADATA = {'extra/favicon.ico': {'path': 'favicon.ico'}}
+STATIC_PATHS = [ 'images',
+                 'extra',]
+
+EXTRA_PATH_METADATA = {'extra/favicon.ico': {'path': 'favicon.ico'},}
